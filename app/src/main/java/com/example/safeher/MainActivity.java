aa500bf5846307244626d9dd0d127835caed389c
Abdul1028 <rasoolas2003@gmail.com>
2025-04-12 04:23:09 +0530

    update:minor changes

--- app/src/main/java/com/example/safeher/MainActivity.java
+++ app/src/main/java/com/example/safeher/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.safeher;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -10,11 +12,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "AppPrefs";
+    private static final String ONBOARDING_COMPLETED_KEY = "onboardingCompleted";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(MainActivity.this, DashBoardActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
@@ -23,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         findViewById(R.id.btnLogin).setOnClickListener(v->{startActivity(new Intent(MainActivity.this,LoginActivity.class));});
-
 
     }
 }
